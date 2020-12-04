9eca30bd9fd01a5c1a8e8b6921526415297d4419
krzysiek9966 <cmielkrzysztof@gmail.com>
2020-12-04 21:55:23 +0100

    New solutions

--- src/com/example/lab6/producerconsumerwait/ProducerConsumerWait.java
+++ src/com/example/lab6/producerconsumerwait/ProducerConsumerWait.java
@@ -1,18 +1,16 @@
-package com.example.lab6.producerconsumersleepmodified;
+package com.example.lab6.producerconsumerwait;
+
 import java.util.LinkedList;
 import java.util.Random;
 
-public class ProducerConsumerSleepModified {
+public class ProducerConsumerWait {
     static LinkedList<String> buffer = new LinkedList<>();
     static final int BUFFER_SIZE = 10;
-
     public static void main(String[] args) {
-
         class Producer extends Thread {
             private String name;
             public Producer(String name) {
                 this.name = name;
-//                this.setPriority(Thread.MAX_PRIORITY);
             }
             @Override
             public void run() {
@@ -20,8 +18,8 @@ public class ProducerConsumerSleepModified {
                     synchronized (buffer) {
                         if (buffer.size() == BUFFER_SIZE) {
                             try {
-                                Thread.sleep(10);
-                                System.out.println("Producer " + name + " is sleeping, full buffer");
+                                buffer.wait();
+                                System.out.println("Producer " + name + " is waiting, full buffer");
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -29,19 +27,23 @@ public class ProducerConsumerSleepModified {
                             Random r = new Random();
                             String[] words = {"aaa", "bbb", "ccc", "ddd", "eee", "fff"};
                             String s = words[r.nextInt(words.length)];
-                            buffer.addLast(s);
                             System.out.println("Producer " + name + " produces: " + s);
+                            buffer.addLast(s);
+                            buffer.notify();
+                        }
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
             }
         }
-
         class Consumer extends Thread {
             private String name;
             public Consumer(String name) {
                 this.name = name;
-                this.setPriority(Thread.MIN_PRIORITY);
             }
             @Override
             public void run() {
@@ -49,27 +51,32 @@ public class ProducerConsumerSleepModified {
                     synchronized (buffer) {
                         if (buffer.size() == 0) {
                             try {
-                                Thread.sleep(10);
-                                System.out.println("Consumer " + name + " is sleeping, empty buffer");
+                                buffer.wait();
+                                System.out.println("Consumer " + name + " is waiting, empty buffer");
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
                         } else {
                             String elem = buffer.getFirst();
-                            buffer.removeFirst();
+                            System.out.println(this.getPriority());
                             System.out.println("Consumer" + name + " consumes: " + elem + " and process it to: "
                                     + elem.toUpperCase());
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            buffer.removeFirst();
+                            buffer.notify();
                         }
                     }
                 }
             }
         }
-
         new Consumer("I").start();
         new Producer("I").start();
-        new Consumer("II").start();
-        new Consumer("III").start();
-        new Consumer("IV").start();
-        new Consumer("V").start();
+        new Producer("II").start();
+        new Producer("III").start();
+        new Producer("IV").start();
     }
 }
