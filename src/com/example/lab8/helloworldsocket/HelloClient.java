dc3eabba5300366fe60ae19b9bbf903f6cf37de1
krzysiek9966 <cmielkrzysztof@gmail.com>
2021-01-07 20:45:49 +0100

    New solutions

--- src/com/example/lab8/helloworldsocket/HelloClient.java
+++ src/com/example/lab8/helloworldsocket/HelloClient.java
@@ -9,7 +9,9 @@ import java.net.Socket;
 
 public class HelloClient {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Socket socket = new Socket("127.0.0.1", 6000);
+//        Socket socket = new Socket("127.0.0.1", 6000);
+        Socket socket = new Socket("127.0.0.2", 6000);
+
 //        Socket socket = new Socket(InetAddress.getLocalHost(), 6000);
 
         DataOutputStream output = new DataOutputStream(socket.getOutputStream());
